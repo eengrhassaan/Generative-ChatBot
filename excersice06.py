087e6ec460dcf1f59904eaabf0c1a77cd85f2c67
eengrhassaan <eeng.hassaan@gmail.com>
2021-08-21 13:53:46 +0500

    First Commit
    First Commit to check the respo

---
+++ excersice06.py
@@ -0,0 +1,18 @@
+# Define a function which takes 2 numberic list as an input
+# and find the common element in the list
+
+numberList1 = [1,23,7]
+numberList2 = [2,4,1,6,12,22,23,12]
+
+def findCommonNum(numberList1,numberList2):
+    newList = []
+    for item1 in numberList1:
+        # for item2 in numberList2:
+        #     if(item2 == item1):
+        #         newList.append(item2)
+        #         break
+        if item1 in numberList2:
+            newList.append(item1)
+    return newList
+
+print(findCommonNum(numberList1,numberList2))
